0d94ef972d97a948a81133d02e37ec646bd85011
Eric Jizba <ejizba@u.northwestern.edu>
2013-10-13 14:54:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewsTube::Application.config.secret_key_base = 'af9530e23e2b90d84f81e833d846485c96cba40f57ec98f823492f63dcc34b97df1a399bd2f3b4ba2ec9eb282c2239730dd5abea197e58d119edbb43f1309916'
