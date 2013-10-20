da85a58d87e3305a994baf206ddc9bb1ddbbf6c7
Eric Jizba <ejizba@u.northwestern.edu>
2013-10-20 18:51:11 -0500

    Prominent category video ids

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,45 @@
 function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+   gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
 function onYouTubeApiLoad() {
-    gapi.client.setApiKey('AIzaSyAFxd-832oMCK_33cqsRBBoh7EdYHzV2oM');
+   gapi.client.setApiKey('AIzaSyAFxd-832oMCK_33cqsRBBoh7EdYHzV2oM');
 }
 
-function search() {
-	var q = $('#query').val();
-	var request = gapi.client.youtube.search.list({
-	    q: q,
-	    part: 'snippet'
-	});
-
-	request.execute(onSearchResponse);
+function search(){
+   var q = $('#query').val();
+   var requestCNN = gapi.client.youtube.search.list({
+           q: q,
+   channelId: 'UCupvZG-5ko_eiXAupbDfxWw',
+           video: 'CaptionclosedCaption',
+           part: 'snippet'
+   });
+   var requestNBC = gapi.client.youtube.search.list({
+           q: q,
+   channelId: 'UCeY0bbntWzzVIaj2z3QigXg',
+           video: 'CaptionclosedCaption',
+           part: 'snippet'
+   });
+   requestCNN.execute(onSearchResponse);
+   requestNBC.execute(onSearchResponse);
 }
+
 
 function onSearchResponse(response) {
-    var str = JSON.stringify(response, '', 2);
-    $('#search-container').html('<pre>' + str + '</pre>');
+   var str = JSON.stringify(response, '', 2);
+   var idlist = getVideoId(str);
+  $('#prominent').html('<pre>' + idlist + '</pre>');
+}
+//return a list of videoId separated by a comma
+
+function getVideoId(str){
+   var IDlist=""; 
+   while(str.search("videoId")!=-1){
+           var start = str.search("videoId");
+           var id =  str.substring(start+11,start+22);
+           IDlist =  IDlist.concat(id,",");
+           str = str.slice(start+22);
+   }
+   
+   return IDlist
 }
