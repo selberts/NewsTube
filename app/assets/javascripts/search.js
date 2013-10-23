df306a0c5e614c8b2ad3d229a784b03534db8447
senluchen2015 <senluchen2015@u.northwestern.edu>
2013-10-23 11:49:21 -0500

    begin working on title

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -35,16 +35,32 @@ function onSearchResponse(response) {
   openProminent();
 }
 //return a list of videoId separated by a comma
-
+function getTitle(str){
+  var start = str.search('title":');
+  var end = str.search('description');
+  var title= str.substring(start+10,end-4);
+  return title
+}
 
 function getVideoId(str){
    var IDlist=""; 
    while(str.search("videoId")!=-1){
            var start = str.search("videoId");
            var id =  str.substring(start+11,start+22);
-           IDlist =  IDlist.concat("<div class='vid'><a href='http://www.youtube.com/watch?v=",id,"'><img class='vidImg' src='http://img.youtube.com/vi/",id,"/hqdefault.jpg'/></a></div>");
+           var title= getTitle(str);
+           IDlist =  IDlist.concat(displayVideo(id,title));
            str = str.slice(start+22);
    }
    
    return IDlist
+}
+
+function displayVideo(id, title)
+{
+ return "<div class='vid'>" +
+           "<a href='http://www.youtube.com/watch?v=" + id + "'>" +
+             "<img class='vidImg' src='http://img.youtube.com/vi/" + id + "/hqdefault.jpg'/>" +
+           "</a>" +
+           title +
+         "</div>";
 }
